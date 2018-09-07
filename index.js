a95998f2073aa23d326b032dc8eb5603a6636410
lrvolle <lrvolle@gmail.com>
2018-09-06 20:54:18 -0400

    Add loading animation and favicon. Add some new dependencies to improve directory layout.

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+const favicon = require('express-favicon');
+
+
+
 express()
+	.static('images')
+	.static('stylesheets')
+	.use(favicon(__dirname + '/images/favicon.ico'))
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', __dirname)
 	.set('view engine', 'ejs')
