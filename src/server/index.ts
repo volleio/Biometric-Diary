a909910c4e2eeba7fe56363178b79af132d948ef
lrvolle <lrvolle@gmail.com>
2018-09-13 14:10:51 -0400

    Fix stylesheet serving

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,14 @@
 import * as env from "dotenv";
 import * as express from "express";
+import * as path from "path";
 import * as favicon from "serve-favicon";
 
 env.config();
 const PORT = process.env.PORT || 5000;
 
-express.static("images");
-express.static("stylesheets");
-
 const app = express();
 app.use(favicon(__dirname + "/images/favicon.ico"));
-app.use("/", express.static("stylesheets"));
+app.use("/stylesheets", express.static(path.join(__dirname, "stylesheets")));
 app.set("views", __dirname);
 app.set("view engine", "ejs");
 app.get("/", (req, res) => res.render("pages/index"));
