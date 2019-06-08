19b20209020512ad824185ea3c236a33d52f9362
Lucas Volle <Lucas@Volle.io>
2019-06-08 14:14:54 -0400

    Allow custom mongodb db name for heroku

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 const SESSION_SECRET = process.env.SESSION_SECRET || "session secret";
 const REDIS_URL = process.env.REDIS_URL || "localhost:6379";
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/biometric-diary";
+const MONGODB_DBNAME = process.env.MONGODB_DBNAME || "biometric-diary";
 
 const TYPINGDNA_APIKEY = process.env.TYPINGDNA_APIKEY;
 const TYPINGDNA_APISECRET = process.env.TYPINGDNA_APISECRET;
@@ -146,7 +147,7 @@ mongodbClient.connect(MONGODB_URI, { useNewUrlParser: true }, (err, client) =>
 		process.exit(1);
 	}
 
-	const db = client.db("biometric-diary");
+	const db = client.db(MONGODB_DBNAME);
 	loginDataDb = db.collection("login_data");
 
 	app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
